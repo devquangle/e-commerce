22581f8e123ba94120def40d1b5f204fde9114fd
devquangle <devquangle8386@gmail.com>
2025-12-11 03:16:46 +0700

    Initial frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen">
+        Tailwind is working!
+      </div>
+    </>
+  )
+}
+
+export default App
