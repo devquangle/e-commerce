071944d140b816d01fcd71d8e74ffb41990d81c1
devquangle <devquangle8386@gmail.com>
2026-05-22 17:43:01 +0700

    Refactor GeminiConfig to use external API key from properties file

--- backend/src/main/java/com/dev/backend/config/GeminiConfig.java
+++ backend/src/main/java/com/dev/backend/config/GeminiConfig.java
@@ -1,14 +1,16 @@
 package com.dev.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 import com.google.genai.Client;
 
 @Configuration
 public class GeminiConfig {
-    // @Value("${gemini.api-key}")
-    private String apiKey = "AIzaSyDSUCLmcyo9o04i2HUwZbt-RJ8c_Tvq7ps";
+    @Value("${gemini.api-key}")
+    private String apiKey ;
 
     @Bean
     public Client geminiClient() {
