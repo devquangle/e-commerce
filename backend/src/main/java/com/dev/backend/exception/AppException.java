fb2a853c0f4b3215e4645b17c60834c4f1194758
devquangle <devquangle8386@gmail.com>
2026-06-19 14:55:19 +0700

    refactor: simplify AppException constructors and remove unused import in GlobalExceptionHandler

--- backend/src/main/java/com/dev/backend/exception/AppException.java
+++ backend/src/main/java/com/dev/backend/exception/AppException.java
@@ -1,24 +1,25 @@
 package com.dev.backend.exception;
 
 import lombok.Getter;
-
 @Getter
 public class AppException extends RuntimeException {
+
     private final int code;
     private final String error;
-    private final Object data; 
+    private final Object data;
 
     public AppException(int code, String message) {
-        super(message);
-        this.code = code;
-        this.error = null;
-        this.data = null;
+        this(code, message, null, null);
     }
 
     public AppException(int code, String message, String error) {
+        this(code, message, error, null);
+    }
+
+    public AppException(int code, String message, String error, Object data) {
         super(message);
         this.code = code;
         this.error = error;
-        this.data = null;
+        this.data = data;
     }
 }
